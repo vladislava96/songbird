55bdd1e1bdad9c0fb4f66eb237a669fcd9adbf86
ULADZISLAVA <vladislava96.96@mail.ru>
2022-11-14 13:21:16 +0300

    init: start songbird task

---
+++ songbird/js/audio-player.js
@@ -0,0 +1,77 @@
+export default class Player {
+  constructor(s, place) {
+    this.p = document.createElement('div');
+    this.p.className = 'player';
+
+    this.play = document.createElement('button');
+    this.play.classList = 'play player-icon';
+
+    this.progress = document.createElement('input');
+    this.progress.type = "range";
+    this.progress.value = "0";
+    this.progress.min = "0";
+    this.progress.max = "100";
+    this.progress.step = "0.1";
+    this.progress.className = 'progress'
+
+    this.progressTime = document.createElement('div');
+    this.progressTime.textContent = '00:00 / 00:00';
+    this.progressTime.className = 'progress-time';
+
+    this.p.append(this.play, this.progress, this.progressTime);
+    place.append(this.p);
+
+    this.isPlay = false;
+    this.playNum = 0;
+    this.audio = new Audio();
+    this.audio.src = s;
+    this.timerId = null;
+
+    this.play.addEventListener('click', this.toggleBtn.bind(this));
+    this.audio.addEventListener('timeupdate', this.progressLine, false)
+  }
+
+  playAudio() {
+    this.append = 0;
+
+    if (!this.isPlay) {
+      this.isPlay = true;
+      this.audio.play();
+      this.timerId = setInterval(function() {
+
+        let audioTimeRound = Math.round(this.audio.currentTime);
+        let audioLength = Math.round(this.audio.duration);
+
+        var currentMinutes = Math.floor(audioTimeRound / 60);
+        var currentSeconds = (audioTimeRound - currentMinutes * 60);
+
+        var durationMinutes = Math.floor(audioLength / 60);
+        var durationSeconds = (audioLength - durationMinutes * 60);
+
+        this.progressTime.textContent = `${(currentMinutes < 10 ? '0' : '') + currentMinutes}:${(currentSeconds < 10 ? '0' : '') + currentSeconds} / 
+        ${(durationMinutes < 10 ? '0' : '') + durationMinutes}:${(durationSeconds < 10 ? '0' : '') + durationSeconds}`;
+
+      }, 1000)
+
+    } else {
+      this.isPlay = false;  
+      this.audio.pause();
+      clearInterval(this.timerId) 
+    }
+  }
+  
+  toggleBtn() {
+    this.play.classList.toggle('pause');
+    this.playAudio()
+  }
+
+  progressLine() {
+    const persent = (this.audio.currentTime / this.audio.duration) * 100;
+    this.progress.style.background = `linear-gradient(to right, #386D1D 0%, #386D1D ${persent}%, #FFFFFF ${persent}%, #FFFFFF 100%)`;
+    this.progress.value = persent;
+  }
+}
+
+
+
+
---
+++ songbird/quize.js
@@ -0,0 +1,213 @@
+import birdsData from './js/dirds-data.js';
+import Player from './js/audio-player.js';
+
+const answerOptionsList = document.querySelector('.answer-options__list');
+const questionItems = document.querySelectorAll('.question-item');
+const currentQuestionCol = document.querySelector('.current-question-col');
+
+const currentQuestion = document.querySelector('.current-question');
+const answerOptions = document.querySelector('.answer-options');
+const birdDescription = document.querySelector('.bird-description');
+const nextLevelBtm = document.querySelector('.next-level');
+const endMessage = document.querySelector('.end-message');
+const scoreHtml = document.querySelector('.score');
+
+let LEVEL = 1;
+let ANSWER = '';
+let QUESTION = '';
+let answerData;
+let SCORE = 0;
+
+
+class Sound {
+  constructor(src) {
+      this.sound = document.createElement("audio");
+      this.sound.src = src;
+      this.sound.setAttribute("preload", "auto");
+      this.sound.setAttribute("controls", "none");
+      this.sound.style.display = "none";
+      this.isPlay = true;
+      document.body.appendChild(this.sound);
+  }
+
+  play() {
+      this.isPlay = true;
+      this.sound.play();
+  }
+}
+
+questionItems[LEVEL - 1].classList.add('question-item_active')
+
+function generateAnswers(level) {
+  answerOptionsList.innerHTML = '';
+  
+  for (let birdData of birdsData[level - 1]) {
+    let li = document.createElement('li');
+    li.className = 'answer-options__item';
+    
+    let btn = document.createElement('span');
+    btn.className = 'answer-options__btn';
+    
+    let name = birdData.name;
+    li.append(btn, name);
+    answerOptionsList.appendChild(li);
+  }
+
+}
+
+generateAnswers(LEVEL)
+
+function generateSong(level) {
+  QUESTION = '';
+  let randomNumber = Math.floor(Math.random() * (5 - 1)) + 0;
+  QUESTION = birdsData[level - 1][randomNumber].name;
+  let song = birdsData[level - 1][randomNumber].audio;
+  new Player(song, currentQuestionCol);
+}
+
+generateSong(LEVEL)
+
+let minus = 0;
+let levelScore = 5;
+
+answerOptionsList.addEventListener('click', (event) => {
+  ANSWER = '';
+  ANSWER = event.target.textContent;
+  
+  let btn = event.target.querySelector('span');
+
+  console.log(btn)
+  
+  for (let birdData of birdsData[LEVEL - 1]) {
+    if (birdData.name === ANSWER) {
+      answerData = birdData
+    }
+  }
+
+  if (QUESTION === ANSWER) {
+    console.log("victory")
+    getVictory()
+    
+    SCORE += levelScore;
+    scoreHtml.textContent = 'SCORE:' + SCORE;
+    btn.classList = 'answer-options__btn answer-options__btn_true';
+    let sound = new Sound('./audio/victory.mp3');
+    sound.play();
+
+  } else {
+    console.log("no")
+    minus += 1;
+    levelScore = 5 - minus;
+
+    let sound = new Sound('./audio/false.mp3');
+    sound.play();
+
+    btn.classList = 'answer-options__btn answer-options__btn_false';
+    if (levelScore < 0) {
+      levelScore = 0;
+    }
+  }
+
+  console.log('levelScore:' + levelScore)
+  console.log('SCORE:' + SCORE)
+
+  birdDescription.innerHTML = '';
+
+  const birdImg = document.createElement('img');
+  birdImg.src = answerData.image;
+  birdImg.className = 'bird-description__img';
+
+  const birdName = document.createElement('div');
+  birdName.textContent = ANSWER;
+  birdName.className = 'bird-description__name';
+
+  const birdSpecies = document.createElement('div');
+  birdSpecies.textContent = answerData.species;
+  birdSpecies.className = 'bird-description__species';
+
+  const birdText = document.createElement('div');
+  birdText.textContent = answerData.description;
+  birdText.className = 'bird-description__text';
+
+  birdDescription.append(birdImg, birdName, birdSpecies, birdText);
+  new Player(answerData.audio, birdDescription);
+
+})
+
+const currentQuestionBirdName = document.querySelector('.current-question__bird-name');
+const currentQuestionImg = document.querySelector('.current-question__img');
+
+
+function getVictory() {
+  nextLevelBtm.classList = 'next-level next-level_active';
+  currentQuestionBirdName.textContent = answerData.name;
+  currentQuestionImg.src = answerData.image;
+  nextLevelBtm.disabled = false;
+}
+
+const restart = document.createElement('button');
+
+nextLevelBtm.addEventListener('click', () => {
+  nextLevelBtm.classList = 'next-level';
+  minus = 0;
+  levelScore = 5;
+  
+  console.log("click")
+
+  birdDescription.innerHTML = 'Послушайте плеер. Выберите птицу из списка.';
+  currentQuestionBirdName.textContent = '*****';
+  currentQuestionCol.removeChild(currentQuestionCol.querySelector('.player'))
+  currentQuestionImg.src = './img/bird06.jpg';
+
+  questionItems[LEVEL - 1].classList.remove('question-item_active')
+  console.log(LEVEL)
+
+  LEVEL += 1;
+  console.log(LEVEL)
+
+
+  if (LEVEL === 7) {
+    currentQuestion.style.display = 'none';
+    answerOptions.style.display = 'none';
+    birdDescription.style.display = 'none';
+    nextLevelBtm.style.display = 'none';
+    console.log(SCORE);
+    endMessage.style.display = 'block';
+
+    if (SCORE === 30) {
+      endMessage.textContent = `Поздравляю! Вы набрали максимальный балл.`
+    } else {
+      endMessage.textContent = `Победа! Вы набрали ${SCORE} баллов из возможных 30. Пройти викторину еще раз?`
+      
+      restart.textContent = 'Начать заново';
+      endMessage.appendChild(restart);
+    }
+    
+  } else {
+    generateAnswers(LEVEL);
+    generateSong(LEVEL);
+    questionItems[LEVEL - 1].classList.add('question-item_active');
+
+    nextLevelBtm.disabled = true;
+  }
+})
+
+restart.addEventListener('click', () => {
+  LEVEL = 1;
+  ANSWER = '';
+  QUESTION = '';
+  answerData;
+  SCORE = 0;
+
+  questionItems[LEVEL - 1].classList.add('question-item_active');
+  scoreHtml.textContent = 'SCORE:0';
+
+  currentQuestion.style.display = 'grid';
+  answerOptions.style.display = 'block';
+  birdDescription.style.display = 'block';
+  nextLevelBtm.style.display = 'block';
+  endMessage.style.display = 'none';
+
+  generateAnswers(LEVEL)
+  generateSong(LEVEL)
+})
